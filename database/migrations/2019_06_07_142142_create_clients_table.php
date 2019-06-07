102dbcc121a35f36c2be70299d199d9ffb73c79e
Jeremy Reyes B <jjrb6@hotmail.com>
2019-06-07 09:38:04 -0500

    [feat]: Added clients migration.

---
+++ database/migrations/2019_06_07_142142_create_clients_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 50)->nullable(false);
+            $table->string('address', 50)->nullable(false);
+            $table->string('delivery', 45)->nullable(false);
+            $table->unique(['name','address']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
