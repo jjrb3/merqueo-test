28a67e4e37f4bc2acb3ca5a03921f7d96c6fd1e5
Jeremy Reyes B <jjrb6@hotmail.com>
2019-06-07 09:52:33 -0500

    [feat]: Added orders migration.

--- database/migrations/2019_06_07_142408_create_orders_table.php
+++ database/migrations/2019_06_07_142408_create_orders_table.php
@@ -15,7 +15,9 @@ class CreateOrdersTable extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->timestamps();
+            $table->integer('priority')->nullable(false);
+            $table->unsignedBigInteger('client_id');
+            $table->foreign('client_id')->references('id')->on('clients');
         });
     }
 
